d4625262dc43a265e0b93158168fd8e7dd563ad2
Andressa Souza <afariasouza@outlook.com>
2025-02-07 16:13:23 -0300

    Refatoração

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,12 +1,20 @@
+import Conta from "../types/Conta.js";
 import { FormatoData } from "../types/FormatoData.js";
 import { formatarData, formatarMoeda } from "../utils/formatters.js";
-let saldo = 3000;
 const elementoSaldo = document.querySelector('.saldo-valor .valor');
 const elementoDataAcesso = document.querySelector('.block-saldo time');
-if (elementoSaldo != null) {
-    elementoSaldo.textContent = formatarMoeda(saldo);
-}
 if (elementoDataAcesso != null) {
-    const dataAcesso = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_LONGO);
+    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_LONGO);
 }
+renderizarSaldo();
+function renderizarSaldo() {
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
+}
+const SaldoComponent = {
+    atualizar() {
+        renderizarSaldo();
+    }
+};
+export default SaldoComponent;
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,23 +1,37 @@
+import Conta from "../types/Conta.js";
 import { FormatoData } from "../types/FormatoData.js";
 import { formatarData, formatarMoeda } from "../utils/formatters.js";
 
-	let saldo: number = 3000;
 
-	const elementoSaldo = document.querySelector(
-		'.saldo-valor .valor'
-	) as HTMLElement;
-	const elementoDataAcesso = document.querySelector(
-		'.block-saldo time'
-	) as HTMLElement;
+const elementoSaldo = document.querySelector(
+	'.saldo-valor .valor'
+) as HTMLElement;
+const elementoDataAcesso = document.querySelector(
+	'.block-saldo time'
+) as HTMLElement;
 
+
+
+if (elementoDataAcesso != null) {
+	elementoDataAcesso.textContent = formatarData(
+		Conta.getDataAcesso(),
+		FormatoData.DIA_LONGO
+	);
+}
+
+
+renderizarSaldo();
+
+function renderizarSaldo(): void {
 	if (elementoSaldo != null) {
-		elementoSaldo.textContent = formatarMoeda(saldo);
+		elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
 	}
+}
 
-	if (elementoDataAcesso != null) {
-		const dataAcesso: Date = new Date();
-		elementoDataAcesso.textContent = formatarData(
-			dataAcesso,
-			FormatoData.DIA_LONGO
-		);
+const SaldoComponent = {
+	atualizar() {
+		renderizarSaldo();
 	}
+}
+
+export default SaldoComponent;
